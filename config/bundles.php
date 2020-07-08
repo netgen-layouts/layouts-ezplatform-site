0cc533f4f29a88b4284b64e41769dad30c02bc93
Edi Modrić <edi.modric@gmail.com>
2020-07-08 09:31:10 +0200

    Add missing layouts bundles

--- config/bundles.php
+++ config/bundles.php
@@ -54,9 +54,11 @@ return [
     Hautelook\TemplatedUriBundle\HautelookTemplatedUriBundle::class => ['all' => true],
     Netgen\Bundle\ContentBrowserUIBundle\NetgenContentBrowserUIBundle::class => ['all' => true],
     Netgen\Bundle\ContentBrowserBundle\NetgenContentBrowserBundle::class => ['all' => true],
+    Netgen\Bundle\ContentBrowserEzPlatformBundle\NetgenContentBrowserEzPlatformBundle::class => ['all' => true],
     Netgen\Bundle\LayoutsUIBundle\NetgenLayoutsUIBundle::class => ['all' => true],
     Netgen\Bundle\LayoutsAdminBundle\NetgenLayoutsAdminBundle::class => ['all' => true],
     Netgen\Bundle\LayoutsBundle\NetgenLayoutsBundle::class => ['all' => true],
     Netgen\Bundle\LayoutsDebugBundle\NetgenLayoutsDebugBundle::class => ['dev' => true, 'test' => true],
     Netgen\Bundle\LayoutsStandardBundle\NetgenLayoutsStandardBundle::class => ['all' => true],
+    Netgen\Bundle\LayoutsEzPlatformBundle\NetgenLayoutsEzPlatformBundle::class => ['all' => true],
 ];
